f5ecbf526677300df8cb6128efdedbec194ece39
atique-shahriar <satique06@gmail.com>
2024-05-10 01:50:03 +0600

    Add database

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const express = require("express");
 var cors = require("cors");
+const {MongoClient, ServerApiVersion} = require("mongodb");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vbl1j76.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
+async function run() {
+  try {
+    // await client.connect();
+    // await client.db("admin").command({ ping: 1 });
+    const database = client.db("stayInn");
+    const serviceCollection = database.collection("service");
+
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port http://localhost:5000/`);
 });
